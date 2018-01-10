76bf9498cb20eec386b9aae9d4b705325649c554
Auston Stewart <auston@shedbuilt.net>
2018-01-09 20:29:13 -0500

    Updating to 2.2.5

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sed -i 's|usr/bin/env |bin/|' run.sh.in
 ./configure --prefix=/usr \
-            --disable-static
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-install -v -dm755 ${SHED_FAKEROOT}/usr/share/doc/expat-2.2.3
-install -v -m644 doc/*.{html,png,css} ${SHED_FAKEROOT}/usr/share/doc/expat-2.2.3
+            --disable-static || exit 1
+make -j $SHED_NUMJOBS || exit 1
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
+install -v -dm755 ${SHED_FAKEROOT}/usr/share/doc/expat-2.2.5
+install -v -m644 doc/*.{html,png,css} ${SHED_FAKEROOT}/usr/share/doc/expat-2.2.5
