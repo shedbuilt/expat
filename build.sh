12468b194a1c18e44e2f1a4ce9f533a4ba917431
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:23:17 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 sed -i 's|usr/bin/env |bin/|' run.sh.in
 ./configure --prefix=/usr \
             --disable-static || exit 1
-make -j $SHED_NUMJOBS || exit 1
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-install -v -dm755 ${SHED_FAKEROOT}/usr/share/doc/expat-2.2.5
-install -v -m644 doc/*.{html,png,css} ${SHED_FAKEROOT}/usr/share/doc/expat-2.2.5
+make -j $SHED_NUM_JOBS || exit 1
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+install -v -dm755 ${SHED_FAKE_ROOT}/usr/share/doc/expat-2.2.5
+install -v -m644 doc/*.{html,png,css} ${SHED_FAKE_ROOT}/usr/share/doc/expat-2.2.5
