801e7c36af049c04513b7127fd587e8ba0a3dd31
Auston Stewart <auston@shedbuilt.net>
2018-05-30 08:35:29 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-sed -i 's|usr/bin/env |bin/|' run.sh.in
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Configure
+sed -i 's|usr/bin/env |bin/|' run.sh.in &&
 ./configure --prefix=/usr \
-            --disable-static || exit 1
-make -j $SHED_NUM_JOBS || exit 1
+            --disable-static \
+            --docdir="$SHED_PKG_LOCAL_DOCDIR" &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-install -v -dm755 ${SHED_FAKE_ROOT}/usr/share/doc/expat-2.2.5
-install -v -m644 doc/*.{html,png,css} ${SHED_FAKE_ROOT}/usr/share/doc/expat-2.2.5
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    install -v -m644 doc/*.{html,png,css} "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+else
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
