8d18df54ee90823958db783ab10d192ebc88fe4a
Auston Stewart <auston@shedbuilt.net>
2018-08-25 10:46:43 -1000

    Updating to 2.2.6

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
-SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 # Configure
 sed -i 's|usr/bin/env |bin/|' run.sh.in &&
 ./configure --prefix=/usr \
             --disable-static \
-            --docdir="$SHED_PKG_LOCAL_DOCDIR" &&
+            --docdir="$SHED_PKG_DOCS_INSTALL_DIR" &&
 # Build and Install
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
 # Install Documentation
 if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
-    install -v -m644 doc/*.{html,png,css} "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+    install -v -m644 doc/*.{html,png,css} "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
 else
-    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
 fi
